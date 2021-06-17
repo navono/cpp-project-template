013e88fecd46c1148866828603006a4ddd488e20
navono <navono007@gmail.com>
2021-06-17 08:50:36 +0800

    feat: 增加 clang-format

--- app/main.cpp
+++ app/main.cpp
@@ -6,32 +6,36 @@
 #include "doctest.h"
 #endif
 
-#include <iostream>
-#include <stdlib.h>
 #include <fmt/format.h>
+#include <iostream>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
 
-#include "exampleConfig.h"
 #include "example.h"
+#include "exampleConfig.h"
 
 /*
  * Simple main program that demonstrates how access
  * CMake definitions (here the version number) from source code.
  */
 int main() {
-  std::cout << "C++ Boiler Plate v"
-            << PROJECT_VERSION_MAJOR
-            << "."
-            << PROJECT_VERSION_MINOR
-            << "."
-            << PROJECT_VERSION_PATCH
-            << "."
-            << PROJECT_VERSION_TWEAK
-            << std::endl;
+  std::cout << "C++ Boiler Plate v" << PROJECT_VERSION_MAJOR << "."
+            << PROJECT_VERSION_MINOR << "." << PROJECT_VERSION_PATCH << "."
+            << PROJECT_VERSION_TWEAK << std::endl;
 
   std::string name = "World";
   std::cout << fmt::format("Hello: {}!", name) << std::endl;
 
   std::system("cat ../LICENSE");
+
+  // Use the default logger (stdout, multi-threaded, colored)
+  spdlog::info("Hello, {}!", "World");
+
+  // create color multi threaded logger
+  auto console = spdlog::stdout_color_mt("console");
+  auto err_logger = spdlog::stderr_color_mt("stderr");
+  spdlog::get("console")->info("loggers can be retrieved from a global "
+                               "registry using the spdlog::get(logger_name)");
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
